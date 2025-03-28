84801e8746b25242e7d09c92be2553d1d88a74da
Pintea Andrei-Cristian <andreipintea313@gmail.com>
2025-03-28 19:34:11 +0200

    MPIPROJ-68 Managed response models

---
+++ DbProvider/Models/ProviderOutputs/BaseResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace DbProvider.Models.ProviderOutputs;
+
+public class BaseResponse
+{
+    public string Message { get; set; }
+    
+    public bool IsSuccess { get; set; }
+
+
+   
+    public static implicit operator BaseResponse (bool isSuccess) => new BaseResponse(){IsSuccess = isSuccess};
+    public static implicit operator BaseResponse (string message) => new BaseResponse(){Message = message, IsSuccess = false};
+}
