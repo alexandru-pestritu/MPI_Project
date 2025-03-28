c2928e0a688a3960c23d5f020b102ac53ddeaf2e
Pintea Andrei-Cristian <andreipintea313@gmail.com>
2025-03-28 15:59:33 +0200

    MPIPROJ-66 Added route for user registration

---
+++ backend/Models/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Models;
+
+public class RegisterRequest
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    
+    public RegisterRequest(string username, string password, string email)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+    }
+}
