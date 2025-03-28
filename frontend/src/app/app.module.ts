66d2d5b8376d0d1376638f599d8082621742e60b
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-03-28 12:41:02 +0200

    MPIPROJ-8 Login form validation using reactive forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,15 +7,21 @@ import Aura from '@primeng/themes/aura';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    ReactiveFormsModule,
+    InputTextModule
   ],
   providers: [
     provideClientHydration(withEventReplay()),
