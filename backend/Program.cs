5d2ddcc4c7e3b1360aa3438d990113c3bce58267
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2025-03-28 14:04:01 +0200

    MPIPROJ-9 Added Swagger UI

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,35 @@
+using DbProvider.Database;
+using DbProvider.Providers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IDatabaseConnectionDetails>(sp => 
+    new DatabaseConnectionDetails("172.20.100.2", "MPI_Database", "sa", "Password123"));
+
+builder.Services.AddSingleton<IDbManager>(sp =>
+{
+    IDatabaseConnectionDetails? connectionDetails = sp.GetService<IDatabaseConnectionDetails>();
+    if (connectionDetails is null)
+    {
+        throw new InvalidOperationException("Database connection details not found");
+    }
+
+    return new DbManager(connectionDetails);
+});
+
+builder.Services.AddSingleton<IAuthProvider>(sp =>
+{
+    IDbManager? dbManager = sp.GetService<IDbManager>();
+    if (dbManager is null)
+    {
+        throw new InvalidOperationException("Database manager not found");
+    }
+
+    return new AuthProvider(dbManager);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -8,18 +39,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Index}/{id?}");
 
 app.Run();
