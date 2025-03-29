363c590fdfb474e3e452c05ca69a9801eadb7db5
Andrei Tudor <tudor.andrei.707@gmail.com>
2025-03-29 14:24:42 +0200

    MPIPROJ-14 added routes for password management

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
+import {ResetPasswordComponent} from './components/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path:'register', component:RegisterComponent},
+  {path: 'forgot-password', component:ForgotPasswordComponent},
+  {path: 'reset-password/:token', component:ResetPasswordComponent},
 ];
 
 @NgModule({
