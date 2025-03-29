0b6de9fbd3a717241ebbe7093b511b83c2000743
Andrei Tudor <tudor.andrei.707@gmail.com>
2025-03-29 13:59:03 +0200

    MPIPROJ-16 Implemented reset link for password reset

--- backend/Services/IAppEmailService.cs
+++ backend/Services/IAppEmailService.cs
@@ -3,4 +3,5 @@
 public interface IAppEmailService
 {
     Task SendVerificationEmailAsync(string toEmail, string verificationLink);
+    Task SendPasswordResetEmailAsync(string toEmail, string resetLink);
 }
