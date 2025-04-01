53d662e74beb58a5c07cad810de4b5fe3e245b2a
Pintea Andrei-Cristian <andreipintea313@gmail.com>
2025-04-01 15:51:34 +0300

    MPIPROJ-41 Added validations for courses and student management

---
+++ DbProvider/Providers/ICourseProvider.cs
@@ -0,0 +1,18 @@
+﻿using DbProvider.Models;
+using DbProvider.Models.ProviderOutputs;
+
+namespace DbProvider.Providers;
+
+public interface ICourseProvider
+{
+    public Task<List<Course>> GetCourses(User user);
+    public Task<Course> AddCourse(Course course);
+    public Task<BaseResponse> EditCourse(Course course);
+    public Task<BaseResponse> DeleteCourse(int courseId);
+    
+    public Task<BaseResponse> AddStudentToCourse(int courseId, int studentId);
+    
+    public Task<BaseResponse> RemoveStudentFromCourse(int courseId, int studentId);
+    
+    public Task<int> GetTeacherId(int courseId);
+}
