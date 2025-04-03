f050d9f752db5969c05ec8dd921e6229fe771a1f
Pintea Andrei-Cristian <andreipintea313@gmail.com>
2025-04-03 14:08:19 +0300

    MPIPROJ-24 Created add grade endpoint

---
+++ DbProvider/Providers/IGradeProvider.cs
@@ -0,0 +1,14 @@
+﻿using DbProvider.Models;
+
+namespace DbProvider.Providers;
+
+public interface IGradeProvider
+{
+    public Task<List<Grade>> GetGrades(int courseId);
+    
+    public Task<bool> EditGrade(Grade grade);
+    
+    public Task<List<Grade?>> AddGrades(List<Grade> grades);
+    
+    public Task<bool> DeleteGrade(int gradeId);
+}
